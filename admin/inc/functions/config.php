64232b1d86d41759eb5879bae101f90f74c11d56
godson-pius <40927597+godson-pius@users.noreply.github.com>
2022-03-24 01:31:52 +0100

    Changed Database

--- admin/inc/functions/config.php
+++ admin/inc/functions/config.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 
-// define("HOST", "localhost");
-// define("USER", "bcamtxaw_mellon");
-// define("PASSWORD", "100%%mellon");
-// define("DBNAME", "bcamtxaw_mellon");
-
 define("HOST", "localhost");
-define("USER", "root");
-define("PASSWORD", "");
-define("DBNAME", "chasee");
+define("USER", "macaybzv_swiss");
+define("PASSWORD", "100%swiss");
+define("DBNAME", "macaybzv_swiss");
+
+// define("HOST", "localhost");
+// define("USER", "root");
+// define("PASSWORD", "");
+// define("DBNAME", "chasee");
 
 $link = mysqli_connect(HOST, USER, PASSWORD, DBNAME);
 
