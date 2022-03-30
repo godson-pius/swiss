dd04cb6ec20d0b32df29dca8bd40722e23aa102f
godson-pius <40927597+godson-pius@users.noreply.github.com>
2022-03-30 20:56:37 +0100

    Set login email

--- admin/inc/functions/user_func.php
+++ admin/inc/functions/user_func.php
@@ -179,11 +179,8 @@ function user_login($post)
             $user_id = $result['id'];
             $otp = generateNumber(4);
                 $message = "
-                <html lang='en'>
+                <html>
                 <head>
-                    <meta charset='utf-8'>
-                    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
-                    <!-- CSS only -->
                     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
                     <title>Message</title>
                 </head>
