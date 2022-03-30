354fbd4629b61ff2a494a6087ace43f4b88d218d
godson-pius <40927597+godson-pius@users.noreply.github.com>
2022-03-30 22:01:18 +0100

    Got it

--- admin/inc/functions/user_func.php
+++ admin/inc/functions/user_func.php
@@ -186,8 +186,8 @@ function user_login($post)
                 </head>
                 <body>
                     <center>
-                    <div style='background: #f3b30bc7; padding: 1rem; color: #000; border-radius: 0.25rem!important; width: 500px; text-align: center!important; box-shadow: 0 .5rem 1rem rgba(0,0,0,.15)!important;'>
-                    <img src='https://swissapexfinancial.com/media/color-logo.png' width='50' style='border-radius: 0.25rem!important' alt='Swiss Apex Financial'> <br>
+                    <div style='background: #452121; padding: 1rem; color: #fff; border-radius: 0.25rem!important; width: 500px; text-align: center!important; box-shadow: 0 .5rem 1rem rgba(0,0,0,.15)!important;'>
+                    <img src='https://swissapexfinancial.com/media/color-logo.png' width='150' style='border-radius: 0.25rem!important' alt='Swiss Apex Financial'> <br>
             
                         <h2>Dear $userName,</h2>
                         <h3>This is your ONE-TIME-PASSWORD</h3> <hr>
