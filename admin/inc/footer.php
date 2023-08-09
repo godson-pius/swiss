c8ed715b643cd5f01023c77cf56d95e5db9d46dc
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2023-08-09 22:47:26 +0100

    Update footer.php

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -3,7 +3,7 @@
     <div class="row font-size-sm">
      
       <div class="col-sm-6 order-sm-1 text-center text-sm-left">
-        <a class="font-w600" href="https://www.swissapexfinancial.com" target="_blank">Swiss Apex Financial</a> &copy; <span data-toggle="year-copy"></span>
+        <a class="font-w600" href="https://www.swissapexfinancial.com" target="_blank">Horizon Trustco</a> &copy; <span data-toggle="year-copy"></span>
 
     </div>
   </div>
