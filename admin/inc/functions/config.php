42d4539c603bd3090a69b553fa62695c85169ad3
godson-pius <godsonazubuike15@gmail.com>
2023-08-11 09:54:36 +0100

    config update

--- admin/inc/functions/config.php
+++ admin/inc/functions/config.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 
-// define("HOST", "localhost");
-// define("USER", "u264567441_horizon");
-// define("PASSWORD", "100%Horizon");
-// define("DBNAME", "u264567441_horizon");
-
 define("HOST", "localhost");
-define("USER", "root");
-define("PASSWORD", "");
-define("DBNAME", "horizon");
+define("USER", "u264567441_horizon");
+define("PASSWORD", "100%Horizon");
+define("DBNAME", "u264567441_horizon");
+
+// define("HOST", "localhost");
+// define("USER", "root");
+// define("PASSWORD", "");
+// define("DBNAME", "horizon");
 
 $link = mysqli_connect(HOST, USER, PASSWORD, DBNAME);
 
